b97a6ead183a015387194165cf15860642bec9bd
Sergey Shirnin <nexron171@gmail.com>
2019-01-19 17:01:37 +0300

    v2 x2 faster

--- PasswordResolver-iOS/ViewController.swift
+++ PasswordResolver-iOS/ViewController.swift
@@ -39,6 +39,7 @@ class ViewController: UIViewController {
     }
 
     private func resolve(input: String) -> [String] {
+        let startTime = Date()
         guard input.count >= 1 else { return [] }
         var variants: [String] = []
 
@@ -49,27 +50,84 @@ class ViewController: UIViewController {
             interestedList.append(cifers[index])
         }
 
-        func getVariants(symbList: [String], nextIndex: Int) -> [String] {
+        func getVariants(symbList: [String], nextIndex: Int, index: Int? = nil) -> [String] {
             guard nextIndex < interestedList.count else {
                 return symbList
             }
 
             let variants = getVariants(symbList: interestedList[nextIndex], nextIndex: nextIndex + 1)
             var results: [String] = []
-            for symb in symbList {
+            if let index = index {
                 for variant in variants {
-                    results.append(symb+variant)
+                    results.append(symbList[index]+variant)
+                }
+            } else {
+                for symb in symbList {
+                    for variant in variants {
+                        results.append(symb+variant)
+                    }
                 }
             }
+
             return results
         }
 
         guard interestedList.count > 1 else { return interestedList[0] }
-        variants = getVariants(symbList: interestedList[0], nextIndex: 1)
-        return variants
+
+        let queueA = DispatchQueue(label: "qA", qos: .background)
+        let queueB = DispatchQueue(label: "qB", qos: .background)
+        let queueC = DispatchQueue(label: "qC", qos: .background)
+        let queueD = DispatchQueue(label: "qD", qos: .background)
+
+        let groupA = DispatchGroup()
+        let groupB = DispatchGroup()
+        let groupC = DispatchGroup()
+        let groupD = DispatchGroup()
+
+        var resultsA: [String] = []
+        var resultsB: [String] = []
+        var resultsC: [String] = []
+        var resultsD: [String] = []
+
+        groupA.enter()
+        groupB.enter()
+        groupC.enter()
+        groupD.enter()
+
+        queueA.async {
+            resultsA = getVariants(symbList: interestedList[0], nextIndex: 1, index: 0)
+            groupA.leave()
+        }
+        queueB.async {
+            resultsB = getVariants(symbList: interestedList[0], nextIndex: 1, index: 1)
+            groupB.leave()
+        }
+        queueC.async {
+            resultsC = getVariants(symbList: interestedList[0], nextIndex: 1, index: 2)
+            groupC.leave()
+        }
+        queueD.async {
+            resultsD = getVariants(symbList: interestedList[0], nextIndex: 1, index: 3)
+            groupD.leave()
+        }
+
+        groupA.wait()
+        groupB.wait()
+        groupC.wait()
+        groupD.wait()
+
+//        variants = getVariants(symbList: interestedList[0], nextIndex: 1, start: 0, end: 1)
+        let endTime = Date()
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: "Время выполнения", message: "Elapsed: \(endTime.timeIntervalSince(startTime))", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(ac, animated: true, completion: nil)
+        }
+        return resultsA + resultsB + resultsC + resultsD
     }
 
     private func generatePasswords() {
+        passwordsList = []
         let rawPassword = passwordTextField.text!
         let queue = DispatchQueue(label: "Resolve", qos: .background)
         activityIndicator.startAnimating()
